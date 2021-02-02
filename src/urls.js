43fe6ad50b0f98ad2067f89c5cc0c48abebc2e4c
Kennedy Kambo <39052913+IamKennedyKambo@users.noreply.github.com>
2021-02-02 13:14:27 +0300

    Update urls.js

--- src/urls.js
+++ src/urls.js
@@ -1,2 +1,2 @@
-export const base_url = "https://phiniteks-api.xyz/api/v1/";
-export const local_url = "https://2a0cad68b6db.ngrok.io";
+export const base_url = "https://mamafua-api.xyz/";
+export const local_url = "https://mamafua-api.xyz/";
